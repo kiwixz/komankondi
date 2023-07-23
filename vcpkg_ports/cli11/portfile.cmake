7fe4508e6ed9e5f596c81d94ac8f252071622b5f
kiwixz <kiwixz@outlook.com>
2023-07-23 13:20:45 +0200

    fix cli11 debug config not installing

--- vcpkg_ports/cli11/portfile.cmake
+++ vcpkg_ports/cli11/portfile.cmake
@@ -13,6 +13,7 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
 )
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/CLI11")
 vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
