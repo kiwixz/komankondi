a90af5f94115b871685d2e7dbd024650684de7dc
kiwixz <kiwixz@outlook.com>
2023-07-24 22:12:40 +0200

    only compute cache directory once

--- src/utils/path.cpp
+++ src/utils/path.cpp
@@ -6,20 +6,23 @@
 namespace komankondi {
 
 std::filesystem::path get_cache_directory() {
-    std::filesystem::path suffix = "komankondi";
+    static std::filesystem::path r = [] {
+        std::filesystem::path suffix = "komankondi";
 
-    const char* cache_dir = std::getenv("XDG_CACHE_HOME");
-    if (!cache_dir)
-        cache_dir = std::getenv("LOCALAPPDATA");
-    if (cache_dir)
-        return cache_dir / suffix;
+        const char* cache_dir = std::getenv("XDG_CACHE_HOME");
+        if (!cache_dir)
+            cache_dir = std::getenv("LOCALAPPDATA");
+        if (cache_dir)
+            return cache_dir / suffix;
 
-    suffix = ".cache" / suffix;
-    const char* home_dir = std::getenv("HOME");
-    if (home_dir)
-        return home_dir / suffix;
+        suffix = ".cache" / suffix;
+        const char* home_dir = std::getenv("HOME");
+        if (home_dir)
+            return home_dir / suffix;
 
-    return suffix;
+        return suffix;
+    }();
+    return r;
 }
 
 }  // namespace komankondi
